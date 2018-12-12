b424ab8905bc1aa30b9b5bccdeefaac06eed92b5
Daria <d.zaikina@eastbanctech.ru>
2018-12-12 18:07:10 +0700

    adventure first

---
+++ src/adventure-core/adv-root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Tasks from '../adventures/adventures';
+import About from '../about/about';
+import Adventures from '../adventures/adventures';
+
+function Root() {
+  return <Adventures />
+}
+
+export default Root;
---
+++ src/adventures/adventures.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Task from 'tasks/task';
+import createRequest from 'core/adv-create-request';
+import { fetchTasks } from 'core/adv-api-config';
+import classNames from 'utils/class-names';
+import AddTasks from 'add-task/add-task';
+
+function Adventures() {
+    return <div className='adventures'>тут будет блок путешествий</div>
+}
+export default Adventures;
